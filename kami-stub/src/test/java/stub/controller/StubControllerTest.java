a92db3487b5747a6ab4089e01c5a5ad69542064d
kamiyamaR <agechan009@gmail.com>
2022-07-29 16:19:58 +0900

    テストコード(仮)の追加.

---
+++ kami-stub/src/test/java/stub/controller/StubControllerTest.java
@@ -0,0 +1,13 @@
+package stub.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StubControllerTest {
+
+    @Test
+    void test0001() {
+        fail("まだ実装されていません");
+    }
+}
